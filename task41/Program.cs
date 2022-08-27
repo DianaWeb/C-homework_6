8949470c954f99976167adca7c7d5d9d78dbc283
«Diana» <diana-mk@ya.ru>
2022-08-27 14:08:52 +0700

    Выполнили домашнюю работу № 6

---
+++ task41/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+System.Console.WriteLine("Укажите сколько чисел вы введете: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[N];
+int[] arrayNumber = EnterNumber(N);
+HowManyPositiveNumbers(arrayNumber);
+
+int[] EnterNumber(int number)
+{
+	for (int i = 0; i < number; i++)
+	{
+		System.Console.WriteLine("Введите число: ");
+		array[i] = Convert.ToInt32(Console.ReadLine());
+	}
+	System.Console.WriteLine(string.Join(", ", array));
+	return array;
+}
+
+void HowManyPositiveNumbers(int[] array)
+{
+	int count = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] > 0) count++;
+	}
+	System.Console.WriteLine($"Чисел больше нуля: {count}");
+}
---
+++ task43/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5) */
+System.Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double X = FindX(b1, k1, b2, k2);
+double Y = FindY(X, b1, k1);
+if (k1 == k2)
+{
+	Console.WriteLine("Точки пересечения нет, потому что прямые параллельны!");
+}
+else
+{
+	System.Console.WriteLine($"Координаты точки пересечения двух прямых: х = {X}, y = {Y}");
+}
+
+double FindX(double b1, double k1, double b2, double k2)
+{
+	double x = ((b2 - b1) / (k1 - k2));
+	return x;
+}
+
+double FindY(double x, double b1, double k1)
+{
+	double y = (k1 * x + b1);
+	return y;
+}
